fbc7bae34bbac0676ce6ae2e85384535dee9ff3a
Philip Nelson <me@pnelson.ca>
2015-04-15 21:02:26 -0700

    Initial commit.

---
+++ numerizer.go
@@ -0,0 +1,294 @@
+// Package numerizer parses words to integers.
+package numerizer
+
+import (
+	"errors"
+	"strings"
+)
+
+var singleNumbers = map[string]int64{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+var directNumbers = map[string]int64{
+	"ten":       10,
+	"eleven":    11,
+	"twelve":    12,
+	"thirteen":  13,
+	"fourteen":  14,
+	"fifteen":   15,
+	"sixteen":   16,
+	"seventeen": 17,
+	"eighteen":  18,
+	"nineteen":  19,
+	"ninteen":   19, // misspelling
+}
+
+var tenPrefixNumbers = map[string]int64{
+	"twenty":  20,
+	"thirty":  30,
+	"forty":   40,
+	"fourty":  40, // misspelling
+	"fifty":   50,
+	"sixty":   60,
+	"seventy": 70,
+	"eighty":  80,
+	"ninety":  90,
+}
+
+var largeNumbers = map[string]int64{
+	"thousand": 1000,
+	"million":  1000000,
+	"billion":  1000000000,
+	"trillion": 1000000000000,
+}
+
+type item struct {
+	typ itemType
+	key string
+	val int64
+}
+
+type itemType int
+
+const (
+	itemError itemType = iota
+	itemAnd
+	itemZero
+	itemHundred
+	itemSingle
+	itemDirect
+	itemTenPrefix
+	itemLarge
+	itemEOL
+)
+
+func newItem(s string) item {
+	switch s {
+	case "zero":
+		return item{typ: itemZero}
+	case "hundred":
+		return item{typ: itemHundred, key: s, val: 100}
+	case "and":
+		return item{typ: itemAnd}
+	}
+	return newItemFromMap(s)
+}
+
+func newItemFromMap(s string) item {
+	if v, ok := singleNumbers[s]; ok {
+		return item{typ: itemSingle, key: s, val: v}
+	} else if v, ok := directNumbers[s]; ok {
+		return item{typ: itemDirect, key: s, val: v}
+	} else if v, ok := tenPrefixNumbers[s]; ok {
+		return item{typ: itemTenPrefix, key: s, val: v}
+	} else if v, ok := largeNumbers[s]; ok {
+		return item{typ: itemLarge, key: s, val: v}
+	}
+	return item{}
+}
+
+func (i item) String() string {
+	if i.typ == itemEOL {
+		return "EOL"
+	}
+	return i.key
+}
+
+type parser struct {
+	items []item
+	pos   int
+	prev  int64
+	sum   int64
+	err   error
+}
+
+func newParser(s string) *parser {
+	parts := strings.Fields(s)
+	items := make([]item, 0, len(parts))
+	for _, part := range parts {
+		items = append(items, newItem(part))
+	}
+	return &parser{items: items}
+}
+
+func (p *parser) next() item {
+	next := p.peek()
+	p.pos++
+	return next
+}
+
+func (p *parser) peek() item {
+	if p.pos >= len(p.items) {
+		return item{typ: itemEOL}
+	}
+	return p.items[p.pos]
+}
+
+type parseFn func(*parser) parseFn
+
+// Parse parses the provided string to an integer.
+func Parse(s string) (int64, error) {
+	p := newParser(s)
+	for state := parse; state != nil; {
+		state = state(p)
+	}
+	if p.err != nil {
+		return 0, p.err
+	}
+	return p.sum, nil
+}
+
+func parse(p *parser) parseFn {
+	switch i := p.peek(); i.typ {
+	case itemZero:
+		return parseZero
+	case itemSingle:
+		return parseSingle
+	case itemDirect:
+		return parseDirect
+	case itemTenPrefix:
+		return parseTenPrefix
+	}
+	return parseError
+}
+
+func parseZero(p *parser) parseFn {
+	p.next()
+	if p.peek().typ != itemEOL {
+		return parseError
+	}
+	return nil
+}
+
+func parseSingle(p *parser) parseFn {
+	i := p.next()
+	next := p.peek()
+	switch next.typ {
+	case itemHundred:
+		p.sum += p.prev
+		p.prev = i.val
+		return parseHundred
+	case itemLarge:
+		p.prev = i.val
+		return parseLarge
+	case itemEOL:
+		p.sum += p.prev + i.val
+		return nil
+	}
+	return parseError
+}
+
+func parseDirect(p *parser) parseFn {
+	i := p.next()
+	next := p.peek()
+	switch next.typ {
+	case itemHundred:
+		p.prev = i.val
+		return parseLarge
+	case itemLarge:
+		p.prev = i.val
+		return parseLarge
+	case itemEOL:
+		p.sum += p.prev + i.val
+		return nil
+	}
+	return parseError
+}
+
+func parseTenPrefix(p *parser) parseFn {
+	i := p.next()
+	next := p.peek()
+	switch next.typ {
+	case itemSingle:
+		p.prev += i.val + next.val
+		p.next()
+		ahead := p.peek()
+		switch ahead.typ {
+		case itemHundred:
+			p.sum += p.prev * ahead.val
+			p.prev = 0
+			return parseHundred
+		case itemLarge:
+			return parseLarge
+		default:
+			return parseSingle
+		}
+	case itemLarge:
+		p.prev = i.val
+		return parseLarge
+	case itemEOL:
+		p.sum += p.prev + i.val
+		return nil
+	}
+	return parseError
+}
+
+func parseHundred(p *parser) parseFn {
+	i := p.next()
+	p.prev *= i.val
+	next := p.peek()
+	switch next.typ {
+	case itemSingle:
+		return parseSingle
+	case itemDirect:
+		return parseDirect
+	case itemTenPrefix:
+		return parseTenPrefix
+	case itemLarge:
+		return parseLarge
+	case itemAnd:
+		return parseAnd
+	case itemEOL:
+		p.sum += p.prev
+		return nil
+	}
+	return parseError
+}
+
+func parseLarge(p *parser) parseFn {
+	i := p.next()
+	p.sum += p.prev * i.val
+	p.prev = 0
+	next := p.peek()
+	switch next.typ {
+	case itemSingle:
+		return parseSingle
+	case itemDirect:
+		return parseDirect
+	case itemTenPrefix:
+		return parseTenPrefix
+	case itemAnd:
+		return parseAnd
+	case itemEOL:
+		return nil
+	}
+	return parseError
+}
+
+func parseAnd(p *parser) parseFn {
+	p.next()
+	next := p.peek()
+	switch next.typ {
+	case itemSingle:
+		return parseSingle
+	case itemDirect:
+		return parseDirect
+	case itemTenPrefix:
+		return parseTenPrefix
+	}
+	return parseError
+}
+
+func parseError(p *parser) parseFn {
+	p.err = errors.New("numerizer: parse error")
+	return nil
+}
---
+++ numerizer_test.go
@@ -0,0 +1,92 @@
+package numerizer
+
+import "testing"
+
+func TestParse(t *testing.T) {
+	var tests = []struct {
+		in   string
+		want int64
+	}{
+		{"zero", 0},
+		{"one", 1},
+		{"three", 3},
+		{"eleven", 11},
+		{"seventeen", 17},
+		{"sixty", 60},
+		{"one hundred", 100},
+		{"two hundred", 200},
+		{"three thousand", 3000},
+		{"four million", 4000000},
+		{"five billion", 5000000000},
+		{"six trillion", 6000000000000},
+		{"two hundred four", 204},
+		{"two hundred and four", 204},
+		{"seventeen hundred", 1700},
+		{"three thousand four", 3004},
+		{"three thousand and four", 3004},
+		{"three thousand sixteen", 3016},
+		{"three thousand and sixteen", 3016},
+		{"three thousand thirty", 3030},
+		{"three thousand and thirty", 3030},
+		{"three thousand thirty three", 3033},
+		{"three thousand four hundred", 3400},
+		{"three thousand and four hundred", 3400},
+		{"three thousand five hundred one", 3501},
+		{"three thousand five hundred and one", 3501},
+		{"three thousand six hundred twelve", 3612},
+		{"three thousand six hundred and twelve", 3612},
+		{"three thousand six hundred eighty", 3680},
+		{"three thousand six hundred and eighty", 3680},
+		{"three thousand six hundred eighty four", 3684},
+		{"three thousand six hundred and eighty four", 3684},
+		{"ten thousand", 10000},
+		{"twenty thousand", 20000},
+		{"three hundred thousand", 300000},
+		{"forty five", 45},
+		{"forty five hundred", 4500},
+		{"forty five thousand", 45000},
+		{"nine hundred ninety nine thousand nine hundred ninety nine", 999999},
+		{"nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine", 999999999},
+	}
+
+	for _, tt := range tests {
+		have, err := Parse(tt.in)
+		if err != nil {
+			t.Fatalf("Parse(%q) %v", tt.in, err)
+		} else if have != tt.want {
+			t.Errorf("Parse(%q)\nhave %d\nwant %d", tt.in, have, tt.want)
+		}
+	}
+}
+
+func TestParseError(t *testing.T) {
+	var tests = []string{
+		"",
+		"a",
+		"and",
+		"hundred",
+		"thousand",
+		"zero four",
+		"four zero",
+		"five three",
+		"four eight",
+		"eight seventy",
+		"eleven six",
+		"twelve seventeen",
+		"twelve thirty",
+		"forty eighteen",
+		"forty twenty",
+		"five hundred hundred",
+		"six thousand hundred",
+		"seven thousand thousand",
+		"forty five thousand and",
+		"two and three",
+	}
+
+	for _, tt := range tests {
+		have, err := Parse(tt)
+		if err == nil {
+			t.Errorf("Parse(%q)\nhave %d\nwant parse error", tt, have)
+		}
+	}
+}
